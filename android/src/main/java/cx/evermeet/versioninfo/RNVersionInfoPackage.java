2297c91fc08e65e5bceecc8982e034d2f682f2ef
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2018-10-29 00:00:33 -0400

    Android: rename classnames, package name, module name

---
+++ android/src/main/java/cx/evermeet/versioninfo/RNVersionInfoPackage.java
@@ -0,0 +1,26 @@
+package cx.evermeet.versioninfo;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class RNVersionInfoPackage implements ReactPackage {
+	@Override
+	public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+		List<NativeModule> modules = new ArrayList<>();
+
+		modules.add(new RNVersionInfoModule(reactContext));
+
+		return modules;
+	}
+
+	@Override
+	public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+		return Collections.emptyList();
+	}
+}
